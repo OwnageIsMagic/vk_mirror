4ecf9fa9d1ef82a336f49d2dca36d1b7c3e795ef
Investocenka <kilowatt36@yandex.ru>
2015-07-17 12:00:37 +0300

    17-07-2015

--- vk.com/js/al/wkview.js
+++ vk.com/js/al/wkview.js
@@ -2249,7 +2249,7 @@ var WkView = {
                 return [(s[0] - btnW) / 2, 8, 8];
             },
             showdt: 400,
-            className: 'subscribe_post_tt' + (subscribed ? ' subscribe_post_tt__undo' : '')
+            className: 'subscribe_post_tt'
         });
     },
     _eof: 1
