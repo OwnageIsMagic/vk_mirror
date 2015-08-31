4ac5f8e08f00c6b34efd5a054ce86f2a98ca0873
Investocenka <kilowatt36@yandex.ru>
2015-08-31 12:00:56 +0300

    31-08-2015

--- vk.com/js/al/groups_edit.js
+++ vk.com/js/al/groups_edit.js
@@ -1446,6 +1446,8 @@ var GroupsEdit = {
         var st = scrollGetY();
         var lnk = ge(cur.moreLink);
 
+        debugLog(ch, st, lnk);
+
         if (!isVisible(lnk)) return;
         if (st + ch > lnk.offsetTop) {
             cur.showMore();
@@ -2223,12 +2225,8 @@ var GroupsEdit = {
                 ttEl.tt.hide();
             }
     },
-    saveObsceneEdit: function(el) {
-        cur.obscene.words = val(el)
-            .replace(/\&nbsp\;/ig, ' ');
-    },
     saveObsceneWords: function() {
-        var words = cur.obscene.words,
+        var words = val('group_edit_obscene_stopwords'),
             btn = ge('group_save');
         var params = {
             gid: cur.gid,
