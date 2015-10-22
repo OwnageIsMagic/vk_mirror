2c2ed3272f2ca7c6ad9d9e87193d6b0dd8368ce6
Investocenka <kilowatt36@yandex.ru>
2015-10-22 11:00:44 +0300

    22-10-2015 11:00:42

--- vk.com/js/al/videoview.js
+++ vk.com/js/al/videoview.js
@@ -1057,7 +1057,7 @@ var Videoview = {
                 } else {
                     var showControls = '';
                     //var controlsClass = 'mv_controls_shown';
-                    var hasPlbClass = window.Videocat && options.playlistId ? 'mv_has_plb' : '';
+                    var hasPlbClass = Videoview.getPlaylistBlockEl() ? 'mv_has_plb' : '';
                 }
 
 
