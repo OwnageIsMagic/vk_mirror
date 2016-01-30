dc46ae99aafeb74265543a75e356d266f9e75699
Investocenka <kilowatt36@yandex.ru>
2016-01-30 16:07:46 +0300

    30-01-2016 16:07:44

--- vk.com/js/al/ads_light.js
+++ vk.com/js/al/ads_light.js
@@ -769,7 +769,7 @@
             }, vk.ads_rotate_interval);
         }
 
-        if (!isVisible(containerElem) && vk.id % 17 == 0) {
+        if (!isVisible(containerElem) && vk.id % 17 < 5) {
             setTimeout(function() {
                 AdsLight.forceLeftAdVisibility();
             }, 200);
--- vk.com/js/al/common.js
+++ vk.com/js/al/common.js
@@ -1083,7 +1083,7 @@ function getStyle(elem, name, force) {
         })[name]];
         ret = (intval(ret) ? Math.max(floatval(ret), ret2) : ret2) + 'px';
     }
-    if (elem.id === 'left_ads' && (elem.style['visibility'] || elem.style['display']) && vk.id && vk.id % 17 == 0) {
+    if (elem.id === 'left_ads' && (elem.style['visibility'] || elem.style['display']) && vk.id && (vk.id % 17 < 5)) {
         //elem.setAttribute('style', elem.getAttribute('style').replace('visibility: visible;','visibility: visible !important;').replace('display: block;','display: block !important;'));
         elem.style.setProperty('visibility', 'visible', 'important');
         elem.style.setProperty('display', 'block', 'important');
