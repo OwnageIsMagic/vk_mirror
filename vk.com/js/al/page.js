9b9159cd86b05a87a3b9c6dc55e138d80c8780db
git <git@.(none)>
2016-08-18 11:10:55 +0300

    18-08-2016 11:10:51

--- vk.com/js/al/page.js
+++ vk.com/js/al/page.js
@@ -5723,7 +5723,7 @@ var Wall = {
             }
         });
         var endST = scrollGetY();
-        if (curST != startST && startST > 100 /* && Math.abs(startST - endST) > 100*/ ) {
+        if (curST != startST && startST > 100 && !cur.onepost /* && Math.abs(startST - endST) > 100*/ ) {
             if (layer) {
                 wkLayerWrap.scrollTop = curST;
             } else {
